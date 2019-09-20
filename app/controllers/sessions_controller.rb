9d9eb075029a8e4793c4f5a5823983de5c8d29a1
Robin Doirin <robindoirin@gmail.com>
2019-09-20 17:39:20 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
+    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+   redirect_to '/'
   end
 end
